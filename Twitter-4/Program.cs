962bb325495fc1b0e011357d278a336c0661accb
brooke <brookedanae88@gmail.com>
2021-07-17 17:17:09 -0400

    Initial commit of Twitter-4

---
+++ Twitter-4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Twitter_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new List<string> { "code", "doce", "ecod", "framer", "frame" };
+
+            foreach (var item in text)
+            {
+                if (item[0] != item[1])
+                {
+                    text.RemoveAt(1);
+                }
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void removeWord(List<string> removed, int N)
+        {
+            List<string> result = new List<string>();
+
+            HashSet<string> found = new HashSet<string>();
+
+            for (int i = 0; i < N; i++)
+            {
+                string word = removed[i];
+
+                if (!found.Contains(word))
+                {
+                    result.Add(removed[i]);
+                    found.Add(word);
+                }
+            }
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                Console.WriteLine(result[i] + " ");
+            }
+    }
+    }
+}
