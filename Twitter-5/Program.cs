71a4cb836ca5e0b8a51f95ca45bd2a295a04fc2f
brooke <brookedanae88@gmail.com>
2021-07-17 16:13:40 -0400

    finished

--- Twitter-5/Program.cs
+++ Twitter-5/Program.cs
@@ -7,8 +7,10 @@ namespace Twitter_5
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a number: ");
             long n = Convert.ToInt64(Console.ReadLine().Trim());
 
+            Console.WriteLine("Enter a power: ");
             long p = Convert.ToInt64(Console.ReadLine().Trim());
 
             long result = pthFactor(n, p);
@@ -18,40 +20,41 @@ namespace Twitter_5
 
         public static long pthFactor(long n, long p)
         {
+            long maxLimit = n;
 
-            List<long> factors = new List<long>();
+            List<long> firstHalfFactors = new List<long>();
+            firstHalfFactors.Add(1);
 
-            for (int i = 1; i <= n; i++)
+            for (int i = 2; i < maxLimit; i++)
             {
                 if (n % i == 0)
                 {
-                    factors.Add(i);
+                    maxLimit = n / i;
+                    firstHalfFactors.Add(i);
                 }
-
-                factors.Sort();
             }
 
-            foreach (var num in factors)
+            long size = firstHalfFactors.Count;
+
+            if (p <= size)
             {
-                if (p > factors.Count)
-                {
-                    return 0;
-                }
-                else
-                {
-                    return num;
-                }
+                return firstHalfFactors[(int)p - 1];
             }
-
-            //if (p > factors.Count)
-            //{
-            //    return 0;
-            //}
-            //else
-            //{
-            //    var convert = factors.;
-            //    return convert;
-            //}
+            if (p <= 2 * size)
+            {
+                var inverse = firstHalfFactors[(int)(2 * size - p)];
+                if (Math.Pow(firstHalfFactors[(int)size - 1], 2) == n)
+                {
+                    var index = (int)(2 * size - p) - 1;
+                    if (index < 0)
+                    {
+                        return 0;
+                    }
+                    inverse = firstHalfFactors[index];
+                }
+                return n / inverse;
+            }
+            return 0;
         }
     }
 }
